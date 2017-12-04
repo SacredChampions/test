847b8f23cc25932b4afc1da3a783416b69948c77
SacredChampions <sanjeevboo0256@gmail.com>
2017-12-05 02:53:13 +0530

    f

--- DiscordSelfBot.js
+++ DiscordSelfBot.js
@@ -167,7 +167,7 @@ class DiscordSelfBot {
 							? `${msg.member} dropped your name in #${msg.channel.name} in ${msg.guild.name}`
 							: `${msg.author.username} sent a message with your name`, msg.author.displayAvatarURL())
 						.setFooter(`Message dates from ${moment(msg.createdAt).format('MMMM Do YYYY | HH:mm:ss')}`)
-						.setColor(msg.channel.type === 'text' ? msg.member.displayHexColor : '#535B62')
+						.setColor(0x00AE86)
 						.setThumbnail(msg.author.displayAvatarURL())
 						.addField('Message Content', msg.cleanContent.length > 1024 ? msg.cleanContent.slice(0, 1024) : msg.cleanContent)
 						.addField('Message Attachments', msg.attachments.first() && msg.attachments.first().url ? msg.attachments.map(au => au.url) : 'None');
